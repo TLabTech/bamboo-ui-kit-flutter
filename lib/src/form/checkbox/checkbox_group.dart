7e0841388fc235d93caed42116101a3c33ec7678
yusuf <yusuf@tlab.co.id>
2025-03-06 08:35:59 +0700

    checkbox group

---
+++ lib/src/form/checkbox/checkbox_group.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bamboo_ui_kit/core.dart';
+import 'package:flutter_bamboo_ui_kit/src/fondation/tfont.dart';
+import 'package:flutter_bamboo_ui_kit/widgets/checkbox.dart';
+
+class TCheckBoxGroup extends StatefulWidget {
+  final String? label;
+  final TextStyle? labelStyle;
+  final List<String> items;
+  final ValueChanged<List<String>> onChanged;
+  final List<String>? initialValues;
+  final Color? activeColor;
+  final Color? inactiveColor;
+  final Color? borderColor;
+  final bool isEnabled;
+
+  const TCheckBoxGroup({
+    super.key,
+    required this.items,
+    required this.onChanged,
+    this.label,
+    this.labelStyle,
+    this.initialValues,
+    this.activeColor,
+    this.inactiveColor,
+    this.borderColor,
+    this.isEnabled = true,
+  });
+
+  @override
+  State<TCheckBoxGroup> createState() => _TCheckBoxGroupState();
+}
+
+class _TCheckBoxGroupState extends State<TCheckBoxGroup> {
+  late List<String> _selectedItems;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedItems = widget.initialValues ?? [];
+  }
+
+  void _onItemChanged(String item, bool isChecked) {
+    setState(() {
+      if (isChecked) {
+        _selectedItems.add(item);
+      } else {
+        _selectedItems.remove(item);
+      }
+      widget.onChanged(_selectedItems);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (widget.label != null)
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Text(
+              widget.label!,
+              style: widget.labelStyle ??
+                  TFontBold.body.copyWith(
+                    color: HexColor(neutral900),
+                  ),
+            ),
+          ),
+        ...widget.items.map((item) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0),
+            child: TCheckBox(
+              initialValue: _selectedItems.contains(item),
+              onChanged: (isChecked) => _onItemChanged(item, isChecked),
+              label: item,
+              activeColor: widget.activeColor,
+              inactiveColor: widget.inactiveColor,
+              borderColor: widget.borderColor,
+              isEnabled: widget.isEnabled,
+            ),
+          );
+        }),
+      ],
+    );
+  }
+}
